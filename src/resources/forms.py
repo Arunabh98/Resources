a868fcd383143b0caa21909ee1e2aabb0a82e986
Arunabh98 <arunabhghosh98@gmail.com>
2016-05-01 12:58:31 +0530

    Update Create resource form

--- src/resources/forms.py
+++ src/resources/forms.py
@@ -12,6 +12,13 @@ class resourceForm(forms.ModelForm):
 			"resource_type",
 			"upload",
 		]
+		widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Enter Title','class': 'form-control'}),
+            'description': forms.Textarea(attrs={'placeholder': 'Enter description here', 'class': 'form-control'}),
+            'resource_type': forms.Select(attrs={'class': 'form-control'}),
+        	'course_name':forms.Select(attrs={'class': 'form-control'}),
+        	'upload': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
 	upload = forms.FileField(
 			label = 'Select a file',
 			help_text = 'max. 5 megabytes',
